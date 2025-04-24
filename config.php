b24304012fd9156cd7ad13b14c647e54fa9e418e
HuGoTran-dev <tranngochoanganh06@gmail.com>
2025-04-24 08:26:07 +0700

    fix

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
     'base' => '/21/',
-    'assets' => '/21/',
+    'assets' => '/21/assets/',
     'baseURL' => 'http://localhost/21/', 
     'db' => array(
         'host'     => 'localhost',
-        'name'     => 'ProductDB',
+        'name'     => 'productdb',
         'username' => 'root',
         'password' => ''
     )
